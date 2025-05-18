0c17e4a4884b1ac1472ad664775851cfbd362604
tweedyant <tweedy.ant@gmail.com>
2025-05-18 11:46:02 -0500

    output directory added

--- rr-backend/src/main/java/com/roundtriproundtable/App.java
+++ rr-backend/src/main/java/com/roundtriproundtable/App.java
@@ -1,10 +1,13 @@
 package com.roundtriproundtable;
+import static spark.Spark.*; 
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        port(8000)
+        get("/status", (req, res) -> "backend is alive!")
         System.out.println("Hello World!");
     }
 }
